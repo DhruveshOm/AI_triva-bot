2f36f6bdca3439a30ca35f0a088da0142c904de5
Dhruvesh Singh Om <dhruveshsinghom@Dhruveshs-MacBook-Air.local>
2025-04-20 23:43:25 +0530

    commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,38 @@
+// "use client"
+// import React, { useState } from 'react'
+// import SearchSection from './_components/SearchSection'
+// import TemplteListSection from './_components/TemplateListSection'
+
+// function Dashboard() {
+//   const [userSearchInput,setUserSearchInput]=useState<string>()
+//   return (
+//     <div>
+//       {/* Search Sectio */}
+//       <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
+
+
+//       {/* Template List Section */}
+//       <TemplteListSection userSearchInput={userSearchInput}/>
+//     </div>
+//   )
+// }
+
+// export default Dashboard
 "use client"
 import React, { useState } from 'react'
 import SearchSection from './_components/SearchSection'
-import TemplteListSection from './_components/TemplateListSection'
+import TemplateListSection from './_components/TemplateListSection' // Fixed typo in component name
 
 function Dashboard() {
-  const [userSearchInput,setUserSearchInput]=useState<string>()
+  const [userSearchInput, setUserSearchInput] = useState<string>('') // Initialize with empty string instead of undefined
+
   return (
     <div>
-      {/* Search Sectio */}
-      <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
-
+      {/* Search Section */}
+      <SearchSection onSearchInput={(value: string) => setUserSearchInput(value)} />
 
       {/* Template List Section */}
-      <TemplteListSection userSearchInput={userSearchInput}/>
+      <TemplateListSection userSearchInput={userSearchInput} />
     </div>
   )
 }
